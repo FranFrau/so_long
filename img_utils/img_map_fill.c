9d2f2a3d214ccf5073ba4b69c37a26e646057d0e
Francesco Frau <ffrau@e3r9p1.42roma.it>
2022-05-13 16:11:49 +0200

    Update v1.0.5
    - Removed libmlx.dylib
    - Removed norme errors
    - Minor bug fixes

--- img_utils/img_map_fill.c
+++ img_utils/img_map_fill.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   img_map_fill.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ffrau <ffrau@student.42roma.it>            +#+  +:+       +#+        */
+/*   By: ffrau <ffrau@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 18:36:49 by ffrau             #+#    #+#             */
-/*   Updated: 2022/04/06 13:25:47 by ffrau            ###   ########.fr       */
+/*   Updated: 2022/05/13 16:10:21 by ffrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,28 +38,6 @@ void	fill_windows(t_map *map)
 		}
 	}
 }
-/*
-void	fill_windows_2(t_map *map)
-{
-	int		i;
-	int		j;
-	t_win	v;
-	t_img	f;
-
-	i = 0;
-	v = ma
-	while (i < map->row)
-	{
-		j = 0;
-		while (j < map->col)
-		{
-			if (map->map[i][j] == '1')
-				mlx_put_image_to_window(v.mlx, v.win, f.wall, j * 64, i * 64);
-			j++;
-		}
-		i++;
-	}
-}*/
 
 void	set_image_1(t_map *m, int i, int j)
 {
@@ -85,11 +63,62 @@ void	set_image_1(t_map *m, int i, int j)
 	else
 		set_image_2(m, i, j);
 }
-void	set_image_2(t_map *m, int i, int j)
+
+void	set_wall_2(t_map *m, int i, int j)
 {
 	t_img	f;
 	t_win	v;
 	int		h;
+
+	v = m->win;
+	f = m->img;
+	if (i == 0 && j != 0 && j != m->col - 1)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/down.xpm", &h, &h), j * 64, i * 64);
+	else if (i != 0 && i != m->row - 1 && j == 0)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/right.xpm", &h, &h), j * 64, i * 64);
+	else if (i != 0 && i != m->row - 1 && j == m->col - 1)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/wallvs.xpm", &h, &h), j * 64, i * 64);
+	else if (j != 0 && j != m->row - 1 && j % 2 == 0)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/dietro.xpm", &h, &h), j * 64, i * 64);
+	else if (j != 0 && j != m->row - 1 && j % 2 != 0)
+		mlx_put_image_to_window(v.mlx, v.win, f.wall, j * 64, i * 64);
+}
+
+void	set_wall(t_map *m, int i, int j)
+{
+	t_img	f;
+	t_win	v;
+	int		h;
+
+	v = m->win;
+	f = m->img;
+	if (i == 0 && j == 0)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/corner1.xpm", &h, &h), j * 64, i * 64);
+	else if (i == 0 && j == m->col - 1)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/corner2.xpm", &h, &h), j * 64, i * 64);
+	else if (i == m->row - 1 && j == 0)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/corner3.xpm", &h, &h), j * 64, i * 64);
+	else if (i == m->row - 1 && j == m->col - 1)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/corner4.xpm", &h, &h), j * 64, i * 64);
+	else if (i == m->row - 1 && j != 0 && j != m->col - 1)
+		mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, \
+			"img/wall/wallou.xpm", &h, &h), j * 64, i * 64);
+	else
+		set_wall_2(m, i, j);
+}
+
+void	set_image_2(t_map *m, int i, int j)
+{
+	t_img	f;
+	t_win	v;
 
 	v = m->win;
 	f = m->img;
@@ -104,26 +133,5 @@ void	set_image_2(t_map *m, int i, int j)
 		m->y_exit = i;
 	}
 	else if (m->map[i][j] == '1')
-	{
-		if (i == 0 && j == 0)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/corner1.xpm", &h, &h), j * 64, i * 64);
-		else if (i == 0 && j == m->col - 1)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/corner2.xpm", &h, &h), j * 64, i * 64);
-		else if (i == m->row - 1 && j == 0)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/corner3.xpm", &h, &h), j * 64, i * 64);
-		else if (i == m->row - 1 && j == m->col - 1)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/corner4.xpm", &h, &h), j * 64, i * 64);
-		else if (i == m->row - 1 && j != 0 && j != m->col - 1)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/wallou.xpm", &h, &h), j * 64, i * 64);
-		else if (i == 0 && j != 0 && j != m->col - 1)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/down.xpm", &h, &h), j * 64, i * 64);
-		else if (i != 0 && i != m->row - 1 && j == 0)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/right.xpm", &h, &h), j * 64, i * 64);
-		else if (i != 0 && i != m->row - 1 && j == m->col - 1)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/wall/wallvs.xpm", &h, &h), j * 64, i * 64);
-		else if (j != 0 && j != m->row - 1 && j % 2 == 0)
-			mlx_put_image_to_window(v.mlx, v.win, mlx_xpm_file_to_image(v.mlx, "img/dietro.xpm", &h, &h), j * 64, i * 64);
-		else if (j != 0 && j != m->row - 1 && j % 2 != 0)
-			mlx_put_image_to_window(v.mlx, v.win, f.wall, j * 64, i * 64);
-	}
+		set_wall(m, i, j);
 }
